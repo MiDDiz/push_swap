11a99929dc4d8a52c15e78a6d6a840893bd79e84
jnaftana <jnaftana@student.42madrid.com>
2022-10-04 12:24:57 +0200

    preradix

--- src/solver.c
+++ src/solver.c
@@ -6,7 +6,7 @@
 /*   By: jnaftana <jnaftana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 11:40:38 by jnaftana          #+#    #+#             */
-/*   Updated: 2022/10/04 11:02:25 by jnaftana         ###   ########.fr       */
+/*   Updated: 2022/10/04 12:15:26 by jnaftana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,41 @@
 
 int get_max_number(t_stack *stack_a)
 {
-	int i, min;
+	int i, max;
 
 	i = 0;
-	min = *(stack_a->data);
+	max = *(stack_a->data);
 	while (i < stack_a->size)
 	{
-		if (stack_a->data[i] < min)
-			min = stack_a->data[i];
+		if (stack_a->data[i] > max)
+			max = stack_a->data[i];
 		i++;
 	}
-	return (min);
+	return (ft_strlen(ft_itoa(max)));
+}
+// direction 1 : a -> b 2 : b -> a
+void push_on_digit(int digit, t_stack *a, t_stack *b, int direction)
+{
+	int power;
+	int iter;
+
+	power = 1;
+	while (digit--)
+		power *= 10;
+	iter = 0;
+
+	
 }
 
-int solver_stack(t_stack *stack_a, t_stack *stack_b)
+int solver_stack(t_stack *a, t_stack *b)
 {
-	int element_to_push;
-	
-	element_to_push = get_max_number(stack_a);
-	
+	int iterations, iter;
+
+	iterations = get_max_iterations(a);
+	iter = 0;
+	while(iter < iterations)
+	{
+		push_on_digit(iter, a, b);
+	}
 	return (0);
 }
